e1e729b2b7bcc1814cc9d374e4fa9cc5e5bb4384
Marck <mercercountynj@gmail.com>
2016-12-27 16:15:44 -0500

    about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './About.css';
+
 
 const About = () => {
     return (
-      <div className="app">
-        About Us
+      <div id="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h2>About Us</h2>
+            <p>My-Roofing-Contractors are focused on building and repairing quality roofing systems in New Jersey and surrounding areas. We guarantee quality in all our roof repairs and roof replacement and, most importantly, timely results.</p>
+            <h3>Contact Information</h3>
+            <p>191 Parkinson Avenue</p>
+            <p>Hamilton, NJ 08610</p>
+            <p><a href="tel:1-609-516-8946">(609) 516-8946</a></p>
+          </div>
+        </div>
       </div>
     );
   };
